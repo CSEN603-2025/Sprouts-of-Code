f67b4533d824889c8b307430387a9b953a559652
Mohamed Abdelaziz <mada1421995@gmail.com>
2025-05-12 16:26:16 +0300

    farida

--- milestone2/src/pages/FacultyAcademics/FacultyDashboard.jsx
+++ milestone2/src/pages/FacultyAcademics/FacultyDashboard.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from '../../components/shared/Navbar';
 import Sidebar from '../../components/shared/Sidebar';
+import { Link } from 'react-router-dom';
 
 const FacultyDashboard = () => {
   return (
     <div>
       <div className="faculty-dashboard-content">
+        <Link to="/faculty/reports" className="faculty-reports-link">
+          <button className="faculty-reports-btn">View Student Reports</button>
+        </Link>
         {/* Faculty Academic dashboard content goes here */}
       </div>
     </div>
