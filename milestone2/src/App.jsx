79b4d05055e69c5b0eb3221d4d5f0bb5e92b2dc1
ziad walid <ziadwalid2003@gmail.com>
2025-05-12 19:59:54 +0300

    admin appointments routing
    added admin appointments routing to App.jsx

--- milestone2/src/App.jsx
+++ milestone2/src/App.jsx
@@ -22,6 +22,7 @@ import AdminEmployers from './pages/admin/AdminEmployers'
 import AdminInternships from './pages/admin/AdminInternships'
 import InternshipManagement from './pages/internshipOperations/InternshipManagement'
 import AdminEvaluations from './pages/internshipOperations/AdminEvaluations'
+import AdminAppointments from './pages/admin/AdminAppointments'
 
 // Employer Pages
 import EmployerDashboard from './pages/employerInterface/EmployerDashboard'
@@ -155,6 +156,14 @@ const App = () => {
                           element={
                             <ProtectedRoute allowedRoles={['admin']}>
                               <AdminEvaluations />
+                            </ProtectedRoute>
+                          }
+                        />
+                        <Route
+                          path="/admin/appointments"
+                          element={
+                            <ProtectedRoute allowedRoles={['admin']}>
+                              <AdminAppointments />
                             </ProtectedRoute>
                           }
                         />
